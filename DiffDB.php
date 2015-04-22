968b84990dba39b90f057da287293008ef84d024
ryo33 <ryo12redstone@gmail.com>
2015-04-22 22:49:17 +0900

    fix to hash column structures

--- DiffDB.php
+++ DiffDB.php
@@ -12,6 +12,10 @@ class DiffDB{
         $this->tables = [];
     }
 
+    private function hash($text){
+        return hash('sha256', $true);
+    }
+
     function addTable($name, $columns){
         $this->tables[$name] == $columns;
     }
@@ -22,7 +26,7 @@ class DiffDB{
         foreach($new as $key => $column){
             if(array_key_exists($key, $old)){
                 if($old[$key] !== $column){
-                    $change[$key] = $column;
+                    $change[$key] = hash($column);
                 }
                 unset($old[$key]);
             }else{
