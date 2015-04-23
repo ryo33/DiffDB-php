b53ccd9ef393ff4f15add5f1e63819236a4006bc
ryo33 <ryo12redstone@gmail.com>
2015-04-23 18:50:44 +0900

    enable to work

--- DiffDB.php
+++ DiffDB.php
@@ -17,7 +17,11 @@ class DiffDB{
     }
 
     function addTable($name, $columns){
-        $this->tables[$name] == $columns;
+        if(!(strpos($name, '`') or strpos($name, ';'))){
+            $this->tables[$name] = $columns;
+        }else{
+            //TODO throwing exception or return error message
+        }
     }
 
     private function diffTable($old, $new){
@@ -37,12 +41,13 @@ class DiffDB{
     }
 
     function updateDB($truncate=true){
-        $this->con->execute(<<<SQL
+        $new_tables = $this->tables;
+        $this->con->exec(<<<SQL
 CREATE TABLE IF NOT EXISTS `{$this->prefix}table`(
-`name` VARCHAR(64) PRIMARY KEY,
+`name` VARCHAR(64) PRIMARY KEY
 );
 CREATE TABLE IF NOT EXISTS `{$this->prefix}column`(
-`id` INTEGER PRIMARY KEY,
+`id` INTEGER PRIMARY KEY AUTO_INCREMENT,
 `name` VARCHAR(64),
 `structure` VARCHAR(128),
 `table_name` VARCHAR(64)
@@ -50,10 +55,51 @@ CREATE TABLE IF NOT EXISTS `{$this->prefix}column`(
 SQL
         );
         $old_table_names = $this->con->fetchColumnAll("SELECT `name` FROM `{$this->prefix}table`;");
+        $old_tables = [];
         foreach($old_table_names as $old_table_name){
             $columns = $this->con->fetchAll("SELECT `name`, `structure` FROM `{$this->prefix}column` WHERE `table_name` = ?", $old_table_name);
             foreach($columns as $column){
                 $old_tables[$old_table_name][$column['name']] = $column['structure'];
+            }
+        }
+        foreach($old_tables as $key => $old_table){
+            if(array_key_exists($key, $new_tables)){
+                //update table
+                $result = $this->diffTable($old_table, $new_tables[$key]);
+                if(count($result[0]) or count($result[1]) or count($result[2])){
+                    if($truncate){
+                        $this->con->execute("TRUNCATE TABLE `$key`");
+                    }
+                    foreach($result[1] as $deleted_name => $deleted){
+                        $this->con->execute("ALTER TABLE `$key` DROP COLUMN `$deleted_name`;");
+                        $this->con->execute("DELETE FROM `{$this->prefix}column` WHERE `table_name` = ? AND `name` = ?", [$key, $deleted_name]);
+                    }
+                    foreach($result[0] as $added_name => $added){
+                        $this->con->execute("ALTER TABLE `$key` ADD `$added_name` $added;");
+                        $this->con->insert($this->prefix . 'column', ['table_name' => $key, 'name' => $added_name, 'structure' => $added]);
+                    }
+                    foreach($result[2] as $added_name => $added){
+                        $this->con->execute("ALTER TABLE `$key` MODIFY `$added_name` $added;");
+                        $this->con->update($this->prefix . 'column', ['structure' => $added], '`table_name` = ? AND `name` = ?', [$key, $added_name]);
+                    }
+                }
+                unset($new_tables[$key]);
+            }else{
+                //delete table
+                $this->con->execute("DROP TABLE `$key`;");
+                $this->con->execute("DELETE FROM `{$this->prefix}table` WHERE `name` = ?", $key);
+            }
+        }
+        //create table
+        foreach($new_tables as $key => $new_table){
+            $columns = [];
+            foreach($new_table as $key2 => $column){
+                $columns[] = $key2 . ' ' . $column;
+            }
+            $this->con->execute("CREATE TABLE `$key`(" . implode(', ', $columns) . ');');
+            $this->con->insert($this->prefix . 'table', ['name' => $key]);
+            foreach($new_table as $column_name => $column){
+                $this->con->insert($this->prefix . 'column', ['name' => $column_name, 'structure' => $column, 'table_name' => $key]);
             }
         }
     }
